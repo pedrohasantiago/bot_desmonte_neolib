92fdaa12fe50af3a84640ca83475d1eecad2fc72
Pedro Santiago <pedrohasantiago@gmail.com>
2020-09-08 00:57:18 -0300

    Asserting subtree length again

--- bot_desmonte_neolib/nlp.py
+++ bot_desmonte_neolib/nlp.py
@@ -11,7 +11,7 @@ def extract_target_from_tweet(phrase: str) -> str:
     else:
         raise Exception('"Broken" not found in input')
     rights = list(target_token.rights)
-    # assert len(rights) == 1, (f'Expected a single set of children to the right'
-    #                           f' of "desmonte" in "{phrase}", got: {rights}')
+    assert len(rights) == 1, (f'Expected a single set of children to the right'
+                              f' of "desmonte" in "{phrase}", got: {rights}')
     subtree_init, *_, subtree_end = rights[0].subtree
     return doc[subtree_init.i : subtree_end.i + 1].text
