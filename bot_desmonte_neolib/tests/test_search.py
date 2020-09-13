38f4d44aa87831a18be3945d55a13ba1240dbf42
Pedro Santiago <pedrohasantiago@gmail.com>
2020-09-13 08:09:48 -0300

    Mocking only a specific method of spacy.Token

--- bot_desmonte_neolib/tests/test_search.py
+++ bot_desmonte_neolib/tests/test_search.py
@@ -7,7 +7,8 @@ class TestNLP(TestCase):
 
     def test_iter_target_from_all_tweets(self):
         # When nlp.extract_target_from_tweet raises custom error
-        with patch(__name__ + '.nlp.spacy.tokens.Token', return_value=iter([])):
+        with patch(__name__ + '.nlp.spacy.tokens.Token') as mock_token:
+            mock_token.rights = iter([])
             i = search.iter_target_from_all_tweets()
             a_string = next(i)
             if not a_string or not isinstance(a_string, str):
